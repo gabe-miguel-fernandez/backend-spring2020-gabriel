a8021a18176eafb53c59d6d081251a12df16c9bd
Gabriel Fernandez <gabrielf@Gabriels-MacBook-Pro.local>
2020-05-06 17:42:17 -0700

    Added more features to ToDo app

--- projects/todo/index.js
+++ projects/todo/index.js
@@ -30,3 +30,50 @@ if (fs.existsSync(filename)) {
   const converted = JSON.stringify(data);
   fs.writeFileSync(filename, converted, "utf8");
 }
+
+// finished setting up saved file
+
+/* ToDo Routes: 
+    "/" is important because express will interpret it as directory, not a file.
+ */
+app.use("/", express.static("public_html/"));
+
+
+/*
+    Object to receive from front-end for /newNote:
+    {
+      note: String (note's text to save later)
+      author: Null or string (user or owner of note)
+    }
+    Object to send to front-end for /newNote:
+    {
+      saveStatus: Number (0 for saved, 1 for error)
+    }
+ */
+
+ class Note {
+   constructor(note, author) {
+     this.note = note;
+     this.author = author;
+     this.completed_status = false;
+     this.create_date = Date.now();
+   }
+ }
+
+ // ToDo Routes
+app.post("/newNote", (request, response) => {
+  let receivedData = request.body;
+
+  let newNoteObject = new Note(receivedData.note, receivedData.author);
+
+  data.notes.push(newNoteObject);
+
+  let dataToSend = {
+    saveStatus: 0
+  }
+
+  console.log(data);
+
+  response.send(dataToSend);
+
+});
